c9d20e40e6fef6fc85c130c6b44cfd66ff10cbad
LCordial <ebessen@internode.on.net>
2022-10-17 21:19:01 +1030

    feat: Writing Projects to the database

--- data/projects.ts
+++ data/projects.ts
@@ -1,32 +1,29 @@
-// const projects:any = [
-//     {
-//         projectAmount: "2",
-//         project1: {
-//             projectName: "Test Project",
-//             projectLocation: "Adelaide, South Australia",
-//             dueDate: "Sometime",
-//         },
-//         project2: {
-//             projectName: "Project Test",
-//             projectLocation: "Adelaide, South Australia",
-//             dueDate: "Sometime",
-//         }
-//     }
-// ]
+import {addDoc, collection, doc, getDoc, setDoc, getDocs, getFirestore, limit, orderBy, query, startAfter, startAt, updateDoc, where} from 'firebase/firestore';
+import firebase from '../firebase/firebase';
 
-const projects:any = [
-    {
-        projectName: "Test Project",
-        projectID: "1",
-        projectLocation: "Adelaide, South Australia",
-        dueDate: "Sometime"
-    },
-    {
-        projectName: "Test Project 2",
-        projectID: "2",
-        projectLocation: "Adelaide, South Australia",
-        dueDate: "Sometime"
+const firestore = getFirestore(firebase)
+
+export const updateProjectData = async () => {
+    const reference = doc(firestore, "projects", "projectsData");
+    // Creating a snapshot of the data
+    const snapshot = await getDoc(reference);
+    // Creating a local object and pushing the snapshot data into it
+    var object:any = snapshot.data()
+
+    // Looping through the objects array length and pushing 
+    // that data into the local project object ready for exporting
+    for(var i=0; i < object.data.length; i++){
+        projects.push(object.data[i]);
     }
+    console.log(projects)
+}
+
+// Running the function
+updateProjectData();
+
+// Local array for exporting
+const projects:any = [
+
 ]
 
 export default projects
